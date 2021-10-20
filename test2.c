872f7aa8b8d76041ac83fd5a11046fb93a506731
ian <lai.kheng.yan@gmail.com>
2021-10-20 16:26:43 +0800

    i

--- test2.c
+++ test2.c
@@ -1,4 +1,15 @@
-//
-// Created by Ian Lai on 20/10/21.
-//
+#include <stdio.h>
 
+
+int main() {
+    int testarr[2][2] = {
+            {1, 2}, {3, 4}
+
+    };
+//    for (int i = 0; i < 4; ++i) {
+//        printf("%d", testarr[0][i]);
+//    }
+
+    printf("%d",testarr[0][2]);
+    return 0;
+}
