297210580e8c4ec42e5944f0cfcfde6f35fe1caf
ian <lai.kheng.yan@gmail.com>
2021-10-26 15:41:40 +0800

    ˚

--- test.c
+++ test.c
@@ -1,16 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void test(char *str){
-    while(*str!='\0'){
-        printf("%d\n",*str);
-        str++;
+int stringcmp(char *s1, char *s2);
+
+int main(){
+
+    char src[100],target[100];
+
+    puts("Enter a source string:");
+    fgets(src,100,stdin);
+    puts("Enter a target string:");
+    fgets(target,100,stdin);
+    int val;
+    val =stringcmp(src,target);
+    if(val==0){
+        puts("stringcmp(): equal");
+    }else if(val==-1){
+        puts("stringcmp(): less than");
+    }else{
+        puts("stringcmp(): greater than");
     }
-
+    return 0;
 }
 
-int main()
-{
-    char *ptr = "123";
-    test(ptr);
-    return 0;
+int stringcmp(char *s1, char *s2){
+    int s1_len=0;
+    int s2_len=0;
+    int i=0;
+    while (s1[i]!='\0'){
+
+        s1_len=s1[i]+s1_len;
+        i++;
+    }
+    i=0;
+    while (s2[i]!='\0'){
+        s2_len=s2[i]+s2_len;
+
+        i++;
+    }
+  if(s1_len==s2_len){
+      return 0;
+  }else if(s1_len>s2_len){
+      return 1;
+  }else{
+      return -1;
+  }
+
 }
---
+++ test2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int test;
+    char arr[]="hello";
+
+    test = arr[0];
+    printf("%d",test);
+
+
+    return 0;
+}
