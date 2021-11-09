1c950031e071206b6335d766a1ad0fed85bc328e
ian <lai.kheng.yan@gmail.com>
2021-11-09 12:58:02 +0800

    i

--- test2.c
+++ test2.c
@@ -1,20 +1,107 @@
+
+/*custom header*/
+
+/*end_edit*/
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-
-
+#define MAX 100
+typedef struct {
+    char name[20];
+    char telno[20];
+} PhoneBk;
+void printPB(PhoneBk *pb, int size);
+int readin(PhoneBk *pb);
+void search(PhoneBk *pb, int size, char *target);
 int main()
 {
-    bool a = true;
+    PhoneBk s[MAX];
+    char t[20], *p;
+    int size=0, choice;
+    char dummychar;
 
-    bool b;
+    printf("Select one of the following options: \n");
+    printf("1: readin()\n");
+    printf("2: search()\n");
+    printf("3: printPB()\n");
+    printf("4: exit()\n");
+    do {
+        printf("Enter your choice: \n");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                scanf("%c", &dummychar);
+                size = readin(s);
+                break;
+            case 2:
+                scanf("%c", &dummychar);
+                printf("Enter search name: \n");
+                fgets(t, 20, stdin);
+                if (p=strchr(t,'\n')) *p = '\0';
+                search(s,size,t);
+                break;
+            case 3:
+                printPB(s, size);
+                break;
+        }
+    } while (choice < 4);
+    return 0;
+}
+void printPB(PhoneBk *pb, int size)
+{
+    /*edit*/
+    /* Write your code here */
+    if(size==0){
+        printf("“Empty phonebook\n");
+    }else{
 
-    if(b=a)
-    {
-        printf("%d", b);
+        for(int i=0;i<size;i++){
+            printf("Name: %s\n",(pb+i)->name);
+            printf("Telno: %s\n",(pb+i)->telno);
+        }
     }
 
+    /*end_edit*/
+}
+int readin(PhoneBk *pb)
+{
+    /*edit*/
+    /* Write your code here */
+    char usr_name[20];
+    char usr_tel[20];
+    char *p;
+    int size;
+    while(1){
 
-    return 0;
+
+        printf("Enter name:\n");
+
+
+        fgets(pb->name,100,stdin);
+
+        if(strcmp(pb->name,"#")){
+            break;
+        }
+
+        if(p=strchr(pb->name,'\n')) *p='\0';
+        printf("Enter tel:\n");
+        fgets(pb->telno,100,stdin);
+        if(p=strchr(pb->telno,'\n')) *p='\0';
+
+        pb++;
+
+        size++;
+    }
+
+    return size;
+
+
+    /*end_edit*/
+}
+void search(PhoneBk *pb, int size, char *target)
+{
+    /*edit*/
+    /* Write your code here */
+
+
+    /*end_edit*/
 }
