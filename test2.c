80455e26e584402557f83c9cdcfa916a5deb3ec6
ian <lai.kheng.yan@gmail.com>
2021-11-09 10:20:25 +0800

    i

--- test2.c
+++ test2.c
@@ -1,60 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
-char *sweepSpace1(char *str);
-char *sweepSpace2(char *str);
 
 int main()
 {
-    char str[80],str2[80], *p;
+    bool a = true;
 
-    printf("Enter the string: \n");
-    fgets(str, 80, stdin);
-    if (p = strchr(str,'\n')) *p = '\0';
+    bool b;
 
-    strcpy(str2,str);
-    char *temp = sweepSpace1(str);
-    printf("sweepSpace1(): %s\n", temp);
-    printf("sweepSpace2(): %s\n", sweepSpace2(str2));
+    if(b=a)
+    {
+        printf("%d", b);
+    }
+
+
     return 0;
-}
-
-char *sweepSpace1(char *str)
-{
-
-    char *return_str = malloc( sizeof(char) * (80+1) );
-//    char return_str[80];
-    int i = 0, count = 0;
-
-    while (str[i] != '\0')
-    {
-        if (str[i] == ' '){i++;
-        }
-        return_str[count] = str[i];
-        i++;
-        count++;
-    }
-    return_str[count]= '\0';
-    printf("return string = %s\n", return_str);
-    return return_str;
-}
-
-char* sweepSpace2(char *str)
-{
-    char *return_str = malloc( sizeof(char) * (80+1) );
-//    char return_str[80];
-    int i = 0, count = 0;
-
-    while (*(str+i) != '\0')
-    {
-        if (*(str+i) == ' '){i++;
-        }
-        return_str[count] = *(str+i);
-        i++;
-        count++;
-    }
-    return_str[count]= '\0';
-    printf("return string 2 = %s\n", return_str);
-    return return_str;
 }
