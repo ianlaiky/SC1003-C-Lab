107a72401ede2a51598e5ee3311f236ae54b924b
ian <lai.kheng.yan@gmail.com>
2021-11-15 00:08:15 +0800

    i

--- test2.c
+++ test2.c
@@ -5,58 +5,22 @@
 /*end_edit*/
 #include <stdio.h>
 #include <string.h>
-struct student{
-    char name[20]; /* student name */
-    double testScore; /* test score */
-    double examScore; /* exam score */
-    double total;  /* total = (testScore+examScore)/2 */
-};
-double average();
-int main()
-{
-    printf("average(): %.2f\n", average());
-    return 0;
-}
-double average()
-{
-    /*edit*/
-    /* Write your program code here */
 
-    struct student s[50];
-
-    int i=0;
-    char *p;
-    while(1){
-
-
-        printf("Enter student name:\n");
-        fgets(s[i].name,80,stdin);
-
-        if(p=strchr(s[i].name,'\n')) *p='\0';
-        if(strcmp(s[i].name,"END")==0){
-            break;
-        }
-
-        printf("Enter test score:\n");
-        scanf("%lf",&s[i].testScore);
-        printf("Enter exam score:\n");
-        scanf("%lf",&s[i].examScore);
-        s[i].total=(s[i].testScore+s[i].examScore)/2.0;
+int main(){
 
 
 
+    char a[100];
+    int b;
+    char *c=a;
+    int *d=&b;
+
+    scanf("%s %d", a, &b);
+
+    printf("%s %d\n", a, b);
+    printf("%s\n", c);
+    printf("%d\n", *d);
 
 
-        printf("Student %s total = %f",s[i].name,s[i].total);
 
-        scanf("\n");
-
-
-        i++;
-    }
-
-    return 1;
-
-
-    /*end_edit*/
 }
