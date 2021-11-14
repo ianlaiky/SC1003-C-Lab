fbb6ad4270eaf6885d860e54dfe5747e17e58c3c
ian <lai.kheng.yan@gmail.com>
2021-11-14 16:32:18 +0800

    i

--- test2.c
+++ test2.c
@@ -6,10 +6,22 @@
 #define INIT_VALUE 1000
 
 int main(){
+    int in =100;
+    int curr=1;
+    int total=0;
+   while(1){
 
-    char test_string[INIT_VALUE]="hello";
+       total = curr+total;
 
-    printf("%s",test_string);
+       printf("%d\n",curr);
 
+       if(total>in){
+           printf("%d\n",total);
+           break;
+       }
+       curr++;
+
+
+   }
 
 }
